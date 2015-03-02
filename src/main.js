6c1ecd659aff426b9fa8a4c386605360b4e0c426
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-03-01 20:12:38 -0500

    base for the x-tag component

---
+++ src/main.js
@@ -0,0 +1,24 @@
+(function(){  
+
+  // Create your component here
+  // http://x-tags.org/docs
+  
+  xtag.register('x-stub', {
+    lifecycle: {
+      created: function() {},
+      inserted: function() {},
+      removed: function() {},
+      attributeChanged: function() {}
+    }, 
+    events: { 
+    
+    },
+    accessors: {
+      
+    }, 
+    methods: {
+      
+    }
+  });
+
+})();
